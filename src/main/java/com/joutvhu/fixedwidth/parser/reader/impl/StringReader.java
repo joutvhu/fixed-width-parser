139a3c1809c095f3376f841c5b03f159718f5797
Giao Ho <joutvhu@gmail.com>
2020-09-16 15:42:20 +0700

    Read character

--- src/main/java/com/joutvhu/fixedwidth/parser/reader/impl/StringReader.java
+++ src/main/java/com/joutvhu/fixedwidth/parser/reader/impl/StringReader.java
@@ -4,16 +4,28 @@ import com.joutvhu.fixedwidth.parser.reader.FixedWidthReader;
 import com.joutvhu.fixedwidth.parser.support.FixedParseStrategy;
 import com.joutvhu.fixedwidth.parser.support.FixedTypeInfo;
 import com.joutvhu.fixedwidth.parser.support.StringAssembler;
+import com.joutvhu.fixedwidth.parser.util.CommonUtil;
+import jdk.nashorn.internal.runtime.ParserException;
 
-public class StringReader extends FixedWidthReader<String> {
+import java.util.List;
+
+public class StringReader extends FixedWidthReader<Object> {
+    private static final List<Class> TYPES = CommonUtil.listOf(String.class, Character.class, char.class);
+
     public StringReader(FixedTypeInfo info, FixedParseStrategy strategy) {
         super(info, strategy);
-        if (!String.class.equals(info.getType()))
+        if (!TYPES.contains(info.getType()))
             this.skip();
     }
 
     @Override
-    public String read(StringAssembler assembler) {
-        return assembler.get(info);
+    public Object read(StringAssembler assembler) {
+        String value = assembler.get(info);
+        if (!String.class.equals(info.getType())) {
+            if (info.getLength() != 1)
+                throw new ParserException(String.format("Type of %s is char then it's length must be 1."));
+            return CommonUtil.isNotBlank(value) ? value.charAt(0) : null;
+        }
+        return value;
     }
 }
